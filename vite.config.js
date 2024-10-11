d809dc3670d1c696717c648da4281e90687cdd2e
Jakub Hrášek <hrasekj@gmail.com>
2024-10-11 22:01:57 +0200

    fix: fix dev server

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    // temporary workaround
+    // @see https://github.com/denoland/deno/issues/17757
+    watch: {
+      useFsEvents: false,
+      usePolling: true,
+    },
+  },
 })
