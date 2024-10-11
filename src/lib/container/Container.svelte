b634a00e10d38670e42639e9d38c85de44cd8faa
Jakub Hrášek <hrasekj@gmail.com>
2024-10-11 22:39:07 +0200

    refactor: change imports

--- src/lib/container/Container.svelte
+++ src/lib/container/Container.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" context="module">
-  import { TaskStore } from '../core/services/tasks/TaskStore'
-  import { InMemoryTaskClient } from '../infrastructure/tasks/in-memory-client'
+  import { TaskStore } from '$core/services/tasks/TaskStore'
+  import { InMemoryTaskClient } from '$infrastructure/tasks/in-memory-client'
   import { container } from './store'
   ;(async () => {
     const client = new InMemoryTaskClient()
