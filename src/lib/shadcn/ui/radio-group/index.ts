b43d7bcd4e1ff8ca2b29a8ed059ecb326d056df3
Jakub Hrášek <hrasekj@gmail.com>
2024-10-12 21:52:43 +0200

    feat: add radio-group

---
+++ src/lib/shadcn/ui/radio-group/index.ts
@@ -0,0 +1,15 @@
+import { RadioGroup as RadioGroupPrimitive } from 'bits-ui'
+
+import Root from './radio-group.svelte'
+import Item from './radio-group-item.svelte'
+const Input = RadioGroupPrimitive.Input
+
+export {
+  Input,
+  Input as RadioGroupInput,
+  Item,
+  Item as RadioGroupItem,
+  Root,
+  //
+  Root as RadioGroup,
+}
